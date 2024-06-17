4eb18b221118d2ee38dd641639eb56ba68ec93b3
IMX <phtuanh.imx@gmail.com>
2024-06-17 16:25:28 +0700

    Update KeyboardHelper.swift

--- Sources/SwiftUtils/KeyboardHelper/KeyboardHelper.swift
+++ Sources/SwiftUtils/KeyboardHelper/KeyboardHelper.swift
@@ -9,31 +9,29 @@ import UIKit
 import Combine
 
 public class KeyboardHelper {
-    static public let shared = KeyboardHelper()
-
-    private init() {
+    public init() {
         setupNotification()
     }
-
+    
     private var cancellables: Set<AnyCancellable> = []
     private let subject = PassthroughSubject<KeyboardEvent, Never>()
-
+    
     public enum Animation {
         case keyboardWillShow
         case keyboardWillHide
     }
-
+    
     public struct KeyboardEvent {
-        let animation: Animation
-        let keyboardFrame: CGRect
-        let duration: Double
-        let notification: Notification
+        public let animation: Animation
+        public let keyboardFrame: CGRect
+        public let duration: Double
+        public let notification: Notification
     }
-
+    
     deinit {
         cancellables.forEach { $0.cancel() }
     }
-
+    
     private func setupNotification() {
         cancellables.removeAll()
         NotificationCenter.default
@@ -42,7 +40,7 @@ public class KeyboardHelper {
                 self?.handle(animation: .keyboardWillShow, notification: notification)
             }
             .store(in: &cancellables)
-
+        
         NotificationCenter.default
             .publisher(for: UIResponder.keyboardWillHideNotification)
             .sink { [weak self] notification in
@@ -50,20 +48,22 @@ public class KeyboardHelper {
             }
             .store(in: &cancellables)
     }
-
+    
     private func handle(animation: Animation, notification: Notification) {
         guard let userInfo = notification.userInfo,
               let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
               let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
         else { return }
-
+        
         let event = KeyboardEvent(animation: animation, keyboardFrame: keyboardFrame, duration: duration, notification: notification)
         subject.send(event)
     }
-
+    
     public func observeKeyboardEvents(handler: @escaping (KeyboardEvent) -> Void) {
-        subject.sink { event in
-            handler(event)
-        }.store(in: &cancellables)
+        subject
+            .sink { event in
+                handler(event)
+            }
+            .store(in: &cancellables)
     }
 }
